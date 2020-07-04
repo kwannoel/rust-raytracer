86b754bc2af96df3872f30a780ebdfaa48e4620d
Kwan Noel <noelkwan1998@gmail.com>
2020-07-04 11:45:20 +0800

    Complete 2.1
    * Implement program to write `image.ppm`

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
 fn main() {
-    println!("Hello, world!");
+    const IMAGE_WIDTH : i32 = 256;
+    const IMAGE_HEIGHT : i32 = 256;
+    const MAX_COLOUR_VALUE : i32 = 255;
+
+    // PPM specifications are here: http://davis.lbl.gov/Manuals/NETPBM/doc/ppm.html
+    // PPM example: https://en.wikipedia.org/wiki/Netpbm#PPM_example
+
+    // Write "P3" header for PPM format
+    println!("P3\n");
+
+    // Write image width, height
+    println!("{} {}\n", IMAGE_WIDTH, IMAGE_HEIGHT);
+
+    // Write maximum color value
+    println!("{}\n", MAX_COLOUR_VALUE);
+
+    // Write the pixels from top to bottom row
+    for height in (0..IMAGE_HEIGHT - 1).rev() {
+        // Write the pixels for each row from left to right
+        for width in 0..IMAGE_WIDTH {
+            let r : f64 = f64::from(width) / f64::from(IMAGE_WIDTH - 1);
+            let g : f64 = f64::from(width) / f64::from(IMAGE_HEIGHT - 1);
+            let b : f64 = 0.25;
+
+            let ir : i32 = (r * 255.99) as i32;
+            let ig : i32 = (g * 255.99) as i32;
+            let ib : i32 = (b * 255.99) as i32;
+
+            println!("{} {} {}\n", ir, ig, ib)
+        }
+    }
+    eprintln!("done")
 }
